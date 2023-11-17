a79d8d5b1d262e1748cdeb1077e49845f177367a
deepakkumar9470 <dk397787@gmail.com>
2023-11-17 12:23:22 +0530

    add responsive navigation, fixing some style

---
+++ app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu")
+
+menu.addEventListener('click', function(){
+    navBar = document.querySelector(".nav_Bar")
+    navBar.classList.toggle("active")
+});
+
+
+
+
+
+
+
---
+++ loadContent.js
@@ -0,0 +1,13 @@
+// // Fetch and insert header.html
+// fetch('navbar.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('navbar').innerHTML = data;
+//     });
+
+// // Fetch and insert footer.html
+// fetch('footer.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('footer').innerHTML = data;
+//     });
